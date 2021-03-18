2b276ea1795b7b76fff81432fe248f6f56b1ae61
brodys <brody.strock@gmail.com>
2021-03-18 14:35:02 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exercise_6._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
